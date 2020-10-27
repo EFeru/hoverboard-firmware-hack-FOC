76a27b086c2c728105699c3b6a07603ecfc7c53c
Candas1 <candascraig@hotmail.com>
2020-10-27 19:22:23 +0100

    Add function for MAP

--- Inc/defines.h
+++ Inc/defines.h
@@ -181,6 +181,8 @@
 #define MIN3(a, b, c) MIN(a, MIN(b, c))
 #define MAX3(a, b, c) MAX(a, MAX(b, c))
 #define ARRAY_LEN(x) (uint32_t)(sizeof(x) / sizeof(*(x)))
+#define MAP(x, in_min, in_max, out_min, out_max) ((((x - in_min) * (out_max - out_min)) / (in_max - in_min)) + out_min )
+
 
 typedef struct {
   uint16_t dcr; 
