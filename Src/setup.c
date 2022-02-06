50704b33954de3d08d357bffe6f31b5f61dbc1a8
Noololly <oliver@stephan.co.uk>
2022-02-06 12:05:59 +0000

    Reworked the Wii nunchuck driver to make it much more robust.  New features now include:   * Nunchuck can be hot-plugged   * Recovery is made from bad or lost communication   * Nunchuck checked to see if it is initialised (check for all 0x00 or 0xFF) and values     ignored if not to avoid motor glitches   * Initialisation of I2C is made more reliable following guidance of other STM32 users on the web, i.e.     force restart of peripheral after GPIO pin initialisation

--- Src/setup.c
+++ Src/setup.c
@@ -283,17 +283,21 @@ DMA_HandleTypeDef hdma_i2c2_tx;
 
 void I2C_Init(void)
 {
+  /* Initialise I2C2 GPIO pins
+  *  I2C2 GPIO Configuration
+  *  PB10     ------> I2C2_SCL
+  *  PB11     ------> I2C2_SDA
+  */
+  GPIO_InitTypeDef GPIO_InitStruct;
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /* Initialise I2C peripheral */
   __HAL_RCC_I2C2_CLK_ENABLE();
-  __HAL_RCC_DMA1_CLK_ENABLE();
-
-  /* DMA1_Channel4_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 1, 4);
-  HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
-  /* DMA1_Channel5_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 1, 3);
-  HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
-
   hi2c2.Instance = I2C2;
   hi2c2.Init.ClockSpeed = 200000;
   hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
@@ -303,65 +307,53 @@ void I2C_Init(void)
   hi2c2.Init.OwnAddress2 = 0;
   hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  __HAL_RCC_I2C2_FORCE_RESET();
+  __HAL_RCC_I2C2_RELEASE_RESET();
   HAL_I2C_Init(&hi2c2);
 
-  GPIO_InitTypeDef GPIO_InitStruct;
+  /* Peripheral DMA init*/
+/*  __HAL_RCC_DMA1_CLK_ENABLE();
+*/  
+  /* DMA1_Channel4_IRQn interrupt configuration */
+/*  HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 1, 4);
+  HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
+*/
+  /* DMA1_Channel5_IRQn interrupt configuration */
+/*  HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 1, 3);
+  HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
+*/
 
-    __HAL_RCC_DMA1_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-  /* USER CODE BEGIN I2C2_MspInit 0 */
+/*  hdma_i2c2_rx.Instance = DMA1_Channel5;
+  hdma_i2c2_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  hdma_i2c2_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_i2c2_rx.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_i2c2_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_i2c2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+  hdma_i2c2_rx.Init.Mode = DMA_NORMAL;
+  hdma_i2c2_rx.Init.Priority = DMA_PRIORITY_MEDIUM;
+  HAL_DMA_Init(&hdma_i2c2_rx);
 
-  /* USER CODE END I2C2_MspInit 0 */
+  __HAL_LINKDMA(&hi2c2,hdmarx,hdma_i2c2_rx);
+*/
 
-    /**I2C2 GPIO Configuration
-    PB10     ------> I2C2_SCL
-    PB11     ------> I2C2_SDA
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+/*  hdma_i2c2_tx.Instance = DMA1_Channel4;
+  hdma_i2c2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+  hdma_i2c2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_i2c2_tx.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_i2c2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_i2c2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+  hdma_i2c2_tx.Init.Mode = DMA_NORMAL;
+  hdma_i2c2_tx.Init.Priority = DMA_PRIORITY_MEDIUM;
+  HAL_DMA_Init(&hdma_i2c2_tx);
 
-    /* Peripheral clock enable */
-    __HAL_RCC_I2C2_CLK_ENABLE();
-
-    /* Peripheral DMA init*/
-
-    hdma_i2c2_rx.Instance = DMA1_Channel5;
-    hdma_i2c2_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-    hdma_i2c2_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_i2c2_rx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_i2c2_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_i2c2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_i2c2_rx.Init.Mode = DMA_NORMAL;
-    hdma_i2c2_rx.Init.Priority = DMA_PRIORITY_MEDIUM;
-    HAL_DMA_Init(&hdma_i2c2_rx);
-
-    __HAL_LINKDMA(&hi2c2,hdmarx,hdma_i2c2_rx);
-
-    hdma_i2c2_tx.Instance = DMA1_Channel4;
-    hdma_i2c2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-    hdma_i2c2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_i2c2_tx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_i2c2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_i2c2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_i2c2_tx.Init.Mode = DMA_NORMAL;
-    hdma_i2c2_tx.Init.Priority = DMA_PRIORITY_MEDIUM;
-    HAL_DMA_Init(&hdma_i2c2_tx);
-
-    __HAL_LINKDMA(&hi2c2,hdmatx,hdma_i2c2_tx);
-
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(I2C2_EV_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(I2C2_EV_IRQn);
-    HAL_NVIC_SetPriority(I2C2_ER_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(I2C2_ER_IRQn);
-  /* USER CODE BEGIN I2C2_MspInit 1 */
-
-  /* USER CODE END I2C2_MspInit 1 */
-
-
+  __HAL_LINKDMA(&hi2c2,hdmatx,hdma_i2c2_tx);
+*/
+  /* Peripheral interrupt init */
+/*  HAL_NVIC_SetPriority(I2C2_EV_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(I2C2_EV_IRQn);
+  HAL_NVIC_SetPriority(I2C2_ER_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(I2C2_ER_IRQn);
+*/
 }
 
 void MX_GPIO_Init(void) {
