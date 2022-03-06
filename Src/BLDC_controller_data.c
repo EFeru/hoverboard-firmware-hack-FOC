9d9501ad97d8c6f54ee5d2f96d298f15e557f816
EmanuelFeru <aerdronix@gmail.com>
2022-03-06 11:43:50 +0100

    Fix: field weakening #164

--- Src/BLDC_controller_data.c
+++ Src/BLDC_controller_data.c
@@ -3,9 +3,9 @@
  *
  * Code generated for Simulink model 'BLDC_controller'.
  *
- * Model version                  : 1.1296
+ * Model version                  : 1.1297
  * Simulink Coder version         : 8.13 (R2017b) 24-Jul-2017
- * C/C++ source code generated on : Tue Oct 20 17:29:57 2020
+ * C/C++ source code generated on : Sun Mar  6 11:02:11 2022
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex
